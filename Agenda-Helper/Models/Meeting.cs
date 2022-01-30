107ace04c9cb1fc7faf31fdc20699ad6b6bfea58
Steve Kay <stevekay72@gmail.com>
2022-01-30 01:02:54 +0000

    clean up

--- Agenda-Helper/Models/Meeting.cs
+++ Agenda-Helper/Models/Meeting.cs
@@ -12,6 +12,7 @@ namespace AgendaHelper.Models
 
         public Guid Id { get; init; }
         public DateTime Start { get; set; }
+        public DateTime End { get; set; }
         public string Location { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
